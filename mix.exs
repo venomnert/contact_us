3b7a5d60bf41ba4e8409fb7db657d7ed90caefdf
venomnert <neerthigan@gmail.com>
2019-12-12 07:26:13 -0500

    Ensured to use fixed liveview repo

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule ContactUs.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:phoenix_live_view, github: "phoenixframework/phoenix_live_view"},
+      {:phoenix_live_view, "~> 0.4.0"},
     ]
   end
 
